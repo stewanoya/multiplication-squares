6a7f0ed556dd75053ee2b38c168b2b1c99e01ed5
Stew Anoya <stewartanoya@gmail.com>
2024-02-27 23:06:38 -0500

    add dynamic hover colors

--- src/components/segment/segment.component.ts
+++ src/components/segment/segment.component.ts
@@ -1,8 +1,9 @@
-import { AfterContentChecked, AfterViewInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterContentChecked, AfterViewInit, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { SegmentOrientation } from '../../models/consts.model';
 import { NumberCellModel } from '../../models/number-cell.model';
 import { LineSegment } from '../../models/line-segment.model';
 import { CommonModule } from '@angular/common';
+import { BoardModel } from '../../models/board.model';
 
 @Component({
   selector: 'line-segment',
@@ -13,12 +14,22 @@ import { CommonModule } from '@angular/common';
 })
 export class SegmentComponent implements OnInit {
   @Input() orientation: SegmentOrientation | undefined;
+  @Input() game: BoardModel | undefined
   @Input() neighbourCells: NumberCellModel[] = [];
   @Input() firstRow: boolean = false;
   @Input() firstCell: boolean = false;
   @Output() segmentClickEvent = new EventEmitter<LineSegment>();
+  @ViewChild('segmentRef') segmentRef: ElementRef | undefined;
   segment: LineSegment | undefined;
+  hover = false;
   constructor() {
+  }
+
+  addHover() {
+    this.segmentRef?.nativeElement.classList.add(`hover${this.game?.currentPlayerTurn.color.replace('#', '-')}`)
+  }
+  removeHover() {
+    this.segmentRef?.nativeElement.classList.remove(`hover${this.game?.currentPlayerTurn.color.replace('#', '-')}`)
   }
 
   ngOnInit(): void {
